a7aaee060b81dc51a777a1f730627c7beef0dcc6
赵路 <zhaolu@kuaishou.com>
2019-04-25 12:38:18 +0800

    add python 3 method parse

--- hackerrank/Time Delta.py
+++ hackerrank/Time Delta.py
@@ -6,33 +6,27 @@ import random
 import re
 import sys
 import time
+from dateutil.parser import parse
 
 # Complete the time_delta function below.
 def time_delta(t1, t2):
-    time1 = t1[:-6]
-    time2 = t2[:-6]
-    s1 = time.mktime(time.strptime(time1,"%a %d %b %Y %H:%M:%S"))
-    s2 = time.mktime(time.strptime(time2,"%a %d %b %Y %H:%M:%S"))
-    zm1 = int(t1[-5:-4]+t1[-2:])
-    zm2 = int(t2[-5:-4]+t2[-2:])
-    dms = -(zm1-zm2)*60
-    dhs = -(int(t1[-5:-2])-int(t2[-5:-2]))*3600
+    # time1 = t1[:-6]
+    # time2 = t2[:-6]
+    # s1 = time.mktime(time.strptime(time1,"%a %d %b %Y %H:%M:%S"))
+    # s2 = time.mktime(time.strptime(time2,"%a %d %b %Y %H:%M:%S"))
+    # zm1 = int(t1[-5:-4]+t1[-2:])
+    # zm2 = int(t2[-5:-4]+t2[-2:])
+    # dms = -(zm1-zm2)*60
+    # dhs = -(int(t1[-5:-2])-int(t2[-5:-2]))*3600
 
-    return str(int(abs(s1 - s2 + dms+ dhs)))
+    # return str(int(abs(s1 - s2 + dms+ dhs)))
+
+    t1 = parse(t1)
+    t2 = parse(t2)
+    return int(abs((t1-t2).total_seconds()))
     
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    delta = time_delta('Sun 10 May 2015 13:54:36 -0700', 'Sun 10 May 2015 13:54:36 -0000')
+    print(delta)
 
-    t = int(raw_input())
-
-    for t_itr in xrange(t):
-        t1 = raw_input()
-
-        t2 = raw_input()
-
-        delta = time_delta(t1, t2)
-
-        fptr.write(delta + '\n')
-
-    fptr.close()
